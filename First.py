822856191e44802cd85394497e9b3923a2e10b62
hamidghanbari <gh.hamid89@gmail.com>
2021-07-27 14:12:05 +0200

    Eerste les

--- First.py
+++ First.py
@@ -6,3 +6,17 @@ is_male = True
 print(first_name)
 print(age)
 print(is_male)
+
+phrase = "Hamid"
+phrase2 = phrase + "\tGhanbari"
+print(phrase2)
+
+phrase3 = first_name.lower()
+phrase4 = phrase.upper()
+print(phrase4 +"\n" + phrase3)
+
+phrase5 = phrase.replace("i","I")
+print(phrase5)
+print(len(phrase))
+print(phrase.index("H"))
+print(phrase[2])
